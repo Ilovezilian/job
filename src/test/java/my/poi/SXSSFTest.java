2375d4fba620f9b13310b096af9fc7b698d6c7d4
PanDingrong0129 <1084288424@qq.com>
2020-03-08 15:22:37 +0800

    update

--- src/test/java/my/poi/SXSSFTest.java
+++ src/test/java/my/poi/SXSSFTest.java
@@ -21,61 +21,66 @@ public class SXSSFTest {
 
     @Test
     public void autoFlush() throws Throwable {
-        SXSSFWorkbook wb = new SXSSFWorkbook(100); // keep 100 rows in memory, exceeding rows will be flushed to disk
-        Sheet sh = wb.createSheet();
-        for (int rownum = 0; rownum < 1000; rownum++) {
-            Row row = sh.createRow(rownum);
-            for (int cellnum = 0; cellnum < 10; cellnum++) {
-                Cell cell = row.createCell(cellnum);
-                String address = new CellReference(cell).formatAsString();
-                cell.setCellValue(address);
+        // keep 100 rows in memory, exceeding rows will be flushed to disk
+        try (SXSSFWorkbook wb = new SXSSFWorkbook(100);) {
+            Sheet sh = wb.createSheet();
+            for (int rownum = 0; rownum < 1000; rownum++) {
+                Row row = sh.createRow(rownum);
+                for (int cellnum = 0; cellnum < 10; cellnum++) {
+                    Cell cell = row.createCell(cellnum);
+                    String address = new CellReference(cell).formatAsString();
+                    cell.setCellValue(address);
+                }
             }
-        }
-        // Rows with rownum < 900 are flushed and not accessible
-        for (int rownum = 0; rownum < 900; rownum++) {
-            Assert.assertNull(sh.getRow(rownum));
-        }
-        // ther last 100 rows are still in memory
-        for (int rownum = 900; rownum < 1000; rownum++) {
-            Assert.assertNotNull(sh.getRow(rownum));
-        }
+            // Rows with rownum < 900 are flushed and not accessible
+            for (int rownum = 0; rownum < 900; rownum++) {
+                Assert.assertNull(sh.getRow(rownum));
+            }
+            // ther last 100 rows are still in memory
+            for (int rownum = 900; rownum < 1000; rownum++) {
+                Assert.assertNotNull(sh.getRow(rownum));
+            }
 
-        SXSSF sxssf = new SXSSF();
-        sxssf.setFullFileName("sxssf");
-        FileOutputStream out = new FileOutputStream(sxssf.getFullFileName());
-        wb.write(out);
-        out.close();
-        // dispose of temporary files backing this workbook on disk
-        wb.dispose();
+            SXSSF sxssf = new SXSSF();
+            sxssf.setFullFileName("sxssf");
+            try (FileOutputStream out = new FileOutputStream(sxssf.getFullFileName())) {
+                wb.write(out);
+            }
+
+            // dispose of temporary files backing this workbook on disk
+            wb.dispose();
+        }
     }
 
 
     @Test
     public void manuallyFlush(String[] args) throws Throwable {
-        SXSSFWorkbook wb = new SXSSFWorkbook(-1); // turn off auto-flushing and accumulate all rows in memory
-        SXSSFSheet sh = wb.createSheet();
-        for (int rownum = 0; rownum < 1000; rownum++) {
-            Row row = sh.createRow(rownum);
-            for (int cellnum = 0; cellnum < 10; cellnum++) {
-                Cell cell = row.createCell(cellnum);
-                String address = new CellReference(cell).formatAsString();
-                cell.setCellValue(address);
+        // turn off auto-flushing and accumulate all rows in memory
+        try (SXSSFWorkbook wb = new SXSSFWorkbook(-1)) {
+            SXSSFSheet sh = wb.createSheet();
+            for (int rownum = 0; rownum < 1000; rownum++) {
+                Row row = sh.createRow(rownum);
+                for (int cellnum = 0; cellnum < 10; cellnum++) {
+                    Cell cell = row.createCell(cellnum);
+                    String address = new CellReference(cell).formatAsString();
+                    cell.setCellValue(address);
+                }
+                // manually control how rows are flushed to disk
+                if (rownum % 100 == 0) {
+                    sh.flushRows(100); // retain 100 last rows and flush all others
+                    // ((SXSSFSheet)sh).flushRows() is a shortcut for ((SXSSFSheet)sh).flushRows(0),
+                    // this method flushes all rows
+                }
             }
-            // manually control how rows are flushed to disk
-            if (rownum % 100 == 0) {
-                sh.flushRows(100); // retain 100 last rows and flush all others
-                // ((SXSSFSheet)sh).flushRows() is a shortcut for ((SXSSFSheet)sh).flushRows(0),
-                // this method flushes all rows
-            }
-        }
 
-        SXSSF sxssf = new SXSSF();
-        sxssf.setFullFileName("sxssf");
-        FileOutputStream out = new FileOutputStream(sxssf.getFullFileName());
-        wb.write(out);
-        out.close();
-        // dispose of temporary files backing this workbook on disk
-        wb.dispose();
+            SXSSF sxssf = new SXSSF();
+            sxssf.setFullFileName("sxssf");
+            try (FileOutputStream out = new FileOutputStream(sxssf.getFullFileName())) {
+                wb.write(out);
+            }
+            // dispose of temporary files backing this workbook on disk
+            wb.dispose();
+        }
     }
 
 }
