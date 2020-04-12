5ff092d5a1138afd6b8eabdafffa0af80ba1a5a2
PanDingrong0129 <1084288424@qq.com>
2020-04-12 14:26:04 +0800

    change function variable to function statement

--- web/refacting/firstDemo.js
+++ web/refacting/firstDemo.js
@@ -5,27 +5,28 @@ function volumeCreditsFor(aPerformance) {
     return result;
 }
 
+function getFormat(number) {
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(number);
+}
+
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format;
-
     for (let perf of invoice.performances) {
         volumeCredits = volumeCreditsFor(perf);
 
         // print line for this order
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += ` ${playFor(perf).name}: ${getFormat(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+
+    result += `Amount owed is ${getFormat(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
-
-
     return result;
 }
 
