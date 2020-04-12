36b7b73284122c63f42f5b4eae453df04bea8d30
PanDingrong0129 <1084288424@qq.com>
2020-04-12 14:55:55 +0800

    extract method to calculate total amount and query repalce variable

--- web/refacting/firstDemo.js
+++ web/refacting/firstDemo.js
@@ -21,16 +21,23 @@ function totalVolumeCredits() {
     return volumeCredits;
 }
 
-function statement(invoice, plays) {
+function totalAmount() {
     let totalAmount = 0;
+    for (let perf of invoice.performances) {
+        totalAmount += amountFor(perf);
+    }
+
+    return totalAmount;
+}
+
+function statement(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         // print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 }
