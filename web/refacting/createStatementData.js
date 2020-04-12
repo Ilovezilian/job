66ddd31312341ac4aeed25f0fd67d35dfa8a5b1d
PanDingrong0129 <1084288424@qq.com>
2020-04-13 02:33:08 +0800

    moving volumeCreditsFor fuction into the calculator

--- web/refacting/createStatementData.js
+++ web/refacting/createStatementData.js
@@ -11,7 +11,7 @@ export default function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
@@ -24,10 +24,7 @@ export default function createStatementData(invoice, plays) {
     }
 
     function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
     }
 
     function totalAmount(data) {
@@ -64,6 +61,13 @@ class PerformanceCalculator {
             default:
                 throw new Error(`unknown type: ${this.play.type}`);
         }
+        return result;
+    }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
         return result;
     }
 }
