b19a8a4230885a207aada26e92cdb314a56fa5cf
PanDingrong0129 <1084288424@qq.com>
2020-04-13 00:36:18 +0800

    copy from standard code

--- web/refacting/createStatementData.js
+++ web/refacting/createStatementData.js
@@ -1,10 +1,10 @@
 export default function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
+    const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
+    return result;
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
@@ -47,11 +47,11 @@ export default function createStatementData(invoice, plays) {
         return result;
     }
 
-    function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-    }
-
     function totalAmount(data) {
         return data.performances.reduce((total, p) => total + p.amount, 0);
+    }
+
+    function totalVolumeCredits(data) {
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
 }
